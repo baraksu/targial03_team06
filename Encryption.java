ce5dfd34145bd504402846213f158406a1d6d1ed
Daniel-Behar-blip <danielbehar312@gmail.com>
2025-11-26 14:46:45 +0200

    חילוק של העבודה ובניית הבסיס
    כדי לראות מה אתם עושים ולכאן לראות את הבסיס  READMEתכנסו ל

--- Encryption.java
+++ Encryption.java
@@ -1,16 +1,51 @@
 /**
  * Write a description of class Encryption here.
  *
- * @author (your name)
+ * @author (דניאל בכר,יאיר קוסלובסקי,רון רבי)
  * @version (a version number or a date)
  */
 public class Encryption
 {
     public static void main(String[] args){
-        
         System.out.println("Welcome to the Encryption / Decryption Program");
-        
-    }
+        System.out.println("Enter 1 for Encrypt | 2 for Decrypt");
+        int choice = reader.nextInt();
+        if (choice == 1){
+            System.out.println("Enter up to three words sentence");
+            String sentence = reader.nextLine();
+            int countWords = sentence.split("\\s").length;
+            if (countWords == 1){
+                //יאיר קוסלובסקי
+            }
+            else if (countWords == 2){
+                //רון רבי
+            }
+            else if (countWords == 3){
+                //דניאל בכר
+            }
+            else{
+                System.out.println(sentence + "contains more than 3 words")
+            }
+        }
+        else if (choice == 2){
+            System.out.println("Enter up to three words sentence");
+            String sentence = reader.nextLine();
+            int countWords = sentence.split("\\s").length;
+            if (countWords == 1){
+                //יאיר קוסלובסקי
+            }
+            else if (countWords == 2){
+                //רון רבי
+            }
+            else if (countWords == 3){
+                //דניאל בכר
+            }
+            else{
+                System.out.println(sentence + "contains more than 3 words")
+            }
+        }
+        else
+            System.out.println(choice +" is not a valid choice");
     public static int add (int a, int b){
         return a+b;
     }
