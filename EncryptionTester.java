b2f724e2aed4c6cfe2389f737fe53ac55f7d93f3
Daniel-Behar-blip <danielbehar312@gmail.com>
2025-12-02 16:48:40 +0200

    Update EncryptionTester.java

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -17,15 +17,15 @@ public class EncryptionTester
         assertEquals(6,result);
         String result1 = Encription.main(1,"Hi there you beep");
         assertEquals(result1,"Hi there you beep contains more than three words");
-        String result1 = Encription.main(1,"Hello there you beep");
+        String result2 = Encription.main(1,"Hello there you beep");
         assertEquals(result1,"Hello there you beep contains more than three words");
-        String result1 = Encription.main(2,"Hi there you beep");
+        String result3 = Encription.main(2,"Hi there you beep");
         assertEquals(result1,"Hi there you beep contains more than three words");
-        String result1 = Encription.main(2,"Hello there you beep");
+        String result4 = Encription.main(2,"Hello there you beep");
         assertEquals(result1,"Hello there you beep contains more than three words");
-        String result1 = Encription.main(3);
+        String result5 = Encription.main(3);
         assertEquals(result1,"3 is not a valid choice");
-        String result1 = Encription.main(4);
+        String result6 = Encription.main(4);
         assertEquals(result1,"4 is not a valid choice");
     }
     
