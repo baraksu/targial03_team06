504b02000f92755cb97a494b34dd554fce8911d8
Daniel-Behar-blip <danielbehar312@gmail.com>
2025-12-02 17:38:40 +0200

    Update EncryptionTester.java

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -16,28 +16,24 @@ public class EncryptionTester
         assertEquals(6,result);
     }@Test
     public void testAdd0(){
-        String result1 = Encription.main(1,"Hi there you beep");
-        assertEquals(result1,"Hi there you beep contains more than three words");
+        String result = Encription.Encript3("Hi there you");
+        assertEquals(result,"#r#y0& H1 th");
     }@Test
     public void testAdd1(){
-        String result2 = Encription.main(1,"Hello there you beep");
-        assertEquals(result2,"Hello there you beep contains more than three words");
+        String result = Encription.Encript3(1,"Hello there you");
+        assertEquals(result,"#r#y0& H#ll0 th");
     }@Test
     public void testAdd2(){
-        String result3 = Encription.main(2,"Hi there you beep");
-        assertEquals(result3,"Hi there you beep contains more than three words");
+        String result = Encription.Encript3(2,"Daniel Jacob Behar");
+        assertEquals(result,"c0bB#h@r D@n1#l J@");
     }@Test
     public void testAdd3(){
-        String result4 = Encription.main(2,"Hello there you beep");
-        assertEquals(result4,"Hello there you beep contains more than three words");
+        String result = Encription.Encript3(2,"Daniel my name");
+        assertEquals(result," myn@m# D@n1#l");
     }@Test
     public void testAdd4(){
-        String result5 = Encription.main(3);
-        assertEquals(result5,"3 is not a valid choice");
-    }@Test
-    public void testAdd5(){
-        String result6 = Encription.main(4);
-        assertEquals(result6,"4 is not a valid choice");
+        String result = Encription.Encript3("My name is");
+        assertEquals(result,"@m#1s My n");
     }
     
 }
